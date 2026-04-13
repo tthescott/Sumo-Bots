a9fd56c9d6da1587f7b3a4630679da4eafcd5d41
tthescott <sr24unme@gmail.com>
2026-04-13 15:43:52 -0700

    small optimization for ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -10,12 +10,10 @@ duration = 0.9 # seconds
 try:
     while True:
         bot.drive_forward(speed)
-        distance = bot.get_distance()
 
-        if distance <= stop_distance:
+        if bot.get_distance() <= stop_distance:
             bot.stop_motors()
             bot.turn(speed, duration)
-            bot.stop_motors()
 
 except KeyboardInterrupt:
     bot.stop_motors()
