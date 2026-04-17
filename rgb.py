25dbcf9244c678d06e6f10036cba4252b6316ca1
tthescott <sr24unme@gmail.com>
2026-04-17 12:11:17 -0700

    bot sees red green and blue in rgb.py

---
+++ rgb.py
@@ -0,0 +1,32 @@
+import time
+import board
+import busio
+import adafruit_tcs34725
+
+i2c = board.I2C()
+
+sesnor = adafruit_tcs34725.TCS34725(i2c)
+sesnor.gain = 16
+sesnor.integration_time = 50
+# sesnor.led = False
+
+try:
+  while True:
+    temp = sesnor.color_temperature
+    lux = sesnor.lux
+    rgb = sesnor.color_rgb_bytes
+
+    print(f"Color Temp: {temp}K, Lux: {lux}, RGB: {rgb}")
+    # print(sesnor.color_raw)
+  
+    if (rgb[0] > 2 * rgb[1] and rgb[0] > 2 * rgb[2]):
+      print("red")
+    elif (rgb[1] > 2 * rgb[0] and rgb[1] > 2 * rgb[2]):
+      print("green")
+    elif (rgb[2] > 2 * rgb[0] and rgb[2] > 2 * rgb[1]):
+      print("blue")
+
+    time.sleep(1)
+
+except KeyboardInterrupt:
+  print()
